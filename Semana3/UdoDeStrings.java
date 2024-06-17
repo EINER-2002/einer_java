a0fbcefa48642052cbbdbec86cc44395ac1a21d5
john Doe <202214577i@utea.edu.pe>
2024-06-17 10:17:49 -0500

    Semana3

---
+++ Semana3/UdoDeStrings.java
@@ -0,0 +1,12 @@
+package Semana3;
+
+public class UdoDeStrings {
+    public static void main(String[] args) {
+        String palabra = "cerveza";
+        String frase = "¿donde esta mi cerveza";
+
+        System.out.println("palabra que uso con frecuencia:"+palabra);
+        System.out.println("palabra que uso a veces :"+frase);
+
+    }
+}
